e9a5df932eede1820d2dc9c6c8ce447b90b4ba65
Dave Rintoul <drintoul@gmail.com>
2023-12-22 15:11:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,23 @@
-import streamlit
-import pandas
+import streamlit as st
+import pymysql
 
-st.map(locations)
+# Database connection details
+host = st.secrets["database"]["host"]
+user = st.secrets["database"]["user"]
+password = st.secrets["database"]["password"]
+database = st.secrets["database"]["name"]
+port = st.secrets["database"]["port"]
+
+# Connect to the database
+conn = pymysql.connect(host=host, user=user, password=password, database=database, port=port)
+cursor = conn.cursor()
+
+# Streamlit app
+st.title("MySQL-Streamlit App")
+
+# Example query
+query = "SELECT venue, latitude AS 'lat', longitude AS 'long' FROM venues WHERE latitude IS NOT NULL LIMIT 5"
+cursor.execute(query)
+venues = cursor.fetchall()
+
+st.map(venues)
