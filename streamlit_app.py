03f416a99b64058f5838b403f5d49271c0407b64
Dave Rintoul <drintoul@gmail.com>
2023-12-23 12:06:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ sql = "select v.venue, latitude as 'lat', longitude as 'lon', count(*) as 'count
       "and p.venue = v.venue group by p.venue " \
       "order by count(*) desc limit 10;"
 
-#@st.cache_data(ttl=3600*24)
+@st.cache_data(ttl=3600*24)
 def query():
 
   cursor.execute(sql)
@@ -39,3 +39,32 @@ st.map(venues)
 
 cursor.close()
 cnxn.close()
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None #,
+    #initial_view_state=pdk.ViewState(
+    #    latitude=37.76,
+    #    longitude=-122.4,
+    #    zoom=11,
+    #    pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=venues,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=chart_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
