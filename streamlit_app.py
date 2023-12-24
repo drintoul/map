2263788155c1b080cfe3cef54686573c24c7dfee
Dave Rintoul <drintoul@gmail.com>
2023-12-24 10:37:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ def query():
 
 venues = query()
 
-competitor = st.selectbox('Enter Competitor', ['Julien ALFRED', 'Matthew BOLING', 'Cass ELLIOTT'])
+competitor = st.selectbox('Enter Competitor', ['Udodi Chudi ONWUZURIKE', 'Julien ALFRED', 'Matthew BOLING', 'Cass ELLIOTT'])
 venues = venues[venues['competitor'] == competitor]
 
 st.dataframe(venues, hide_index=True)
