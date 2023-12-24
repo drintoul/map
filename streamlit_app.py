3dd2bb78ab11896d6cda0e8a4989fd1f8fe204ee
Dave Rintoul <drintoul@gmail.com>
2023-12-24 09:04:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,21 +45,18 @@ chart_data = pd.DataFrame(
    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
    columns=['lat', 'lon'])
 
-st.write(chart_data.shape)
-st.dataframe(chart_data.head())
-
 st.pydeck_chart(pdk.Deck(
     map_style=None,
-    initial_view_state=pdk.ViewState(
-        latitude=37.76,
-        longitude=-122.4,
-        zoom=11,
-        pitch=50,
-    ),
+    #initial_view_state=pdk.ViewState(
+    #    latitude=37.76,
+    #    longitude=-122.4,
+    #    zoom=11,
+    #    pitch=50,
+    #),
     layers=[
         pdk.Layer(
            'HexagonLayer',
-           data=chart_data,
+           data=venues,
            get_position='[lon, lat]',
            radius=200,
            elevation_scale=4,
@@ -69,7 +66,7 @@ st.pydeck_chart(pdk.Deck(
         ),
         pdk.Layer(
             'ScatterplotLayer',
-            data=chart_data,
+            data=venues,
             get_position='[lon, lat]',
             get_color='[200, 30, 0, 160]',
             get_radius=200,
